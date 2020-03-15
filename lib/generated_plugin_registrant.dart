c8f0c841d370887f5814658f5bc694ac1070e1de
matejmecka <matej.plavevski+github@gmail.com>
2020-03-15 19:23:18 +0100

    new icon

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,13 +6,11 @@
 import 'dart:ui';
 
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
-  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
 }
