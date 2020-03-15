67a5ca1d822093d447bdb4b552d349f736145921
matejmecka <matej.plavevski+github@gmail.com>
2020-03-15 16:56:18 +0100

    new config

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,12 +5,14 @@
 // ignore: unused_import
 import 'dart:ui';
 
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
 }
